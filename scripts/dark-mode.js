3238b20e4eeca27e5631dff8932491cd8c4c0df4
L <89143664+AraposaFofa@users.noreply.github.com>
2022-10-13 16:00:31 -0300

    Add files via upload

--- scripts/dark-mode.js
+++ scripts/dark-mode.js
@@ -1,5 +1,28 @@
+
+checkDarkMode();
+
+
 let darkToggle2 = document.getElementById('darkToggle')
 
 darkToggle2.addEventListener('change', ()=>{
     document.body.classList.toggle('dark')
+    
 })
+/**
+ *  POR ENQUANTO 
+ * 
+ */
+function checkDarkMode() {
+    if (localStorage.darkMode) {
+        document.body.classList.add('dark')
+    }
+    else{
+        document.body.classList.remove('dark')
+    }
+}
+if (darkToggle2.checked) {
+    localStorage.darkMode = true
+}
+else{
+    localStorage.darkMode = false
+}
